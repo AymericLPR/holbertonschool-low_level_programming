e4bfe02c3c67101b5281b56f3b1f663dfa21bd83
AymericLPR <mric31400@gmail.com>
2022-10-03 07:11:56 -0700

    task 4 C var/if

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -3,27 +3,19 @@
 /**
  * main - entry point
  * Return: Always 0 (Success)
+ * e = 101 and q = 113
  */
 int main(void)
 {
 int alpha = 97;
 while (alpha < 123)
 {
-if (alpha > 96 && alpha < 100)
+if (alpha == 100 || alpha == 112)
 {
 putchar(alpha);
-alpha++;
+alpha+=2;
 }
-if (alpha == 100)
-alpha = 102;
-if (alpha > 101 && alpha < 111)
-{
-putchar(alpha);
-alpha++;
-}
-if (alpha == 111)
-alpha = 113;
-if (alpha > 112)
+if (alpha > 96)
 {
 putchar(alpha);
 alpha++;
