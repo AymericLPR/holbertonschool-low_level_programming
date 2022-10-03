1dff9ea0fae1bceae88be58649deab82546ba0f9
AymericLPR <mric31400@gmail.com>
2022-10-03 06:08:48 -0700

    task 2 C var/if

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int alpha = 97;
+while (alpha < 123)
+{
+putchar(alpha);
+}
+putchar("\n");
+return (0);
+}
