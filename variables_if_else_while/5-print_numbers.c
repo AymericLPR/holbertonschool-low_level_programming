13017210a06cd0e8f04902bfedde5293081e7dd8
AymericLPR <mric31400@gmail.com>
2022-10-04 00:21:04 -0700

    task 5 C var/if

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int nombre = 0
+while (nombre < 10)
+{
+putchar(nombre);
+nombre++;
+}
+putchar('\n');
+return (0);
+}
+    
