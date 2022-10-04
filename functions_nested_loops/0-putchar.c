23207637f649c8cc383496f839f5f3ad9bc25d72
AymericLPR <mric31400@gmail.com>
2022-10-04 05:38:06 -0700

    task 0 C fonction/loop

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Return: 0 no error
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
