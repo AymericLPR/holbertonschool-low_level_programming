b1012679694a6127b07c64e45abf16a6e633a962
AymericLPR <mric31400@gmail.com>
2022-10-04 04:45:24 -0700

    task 6 C var/if

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number = 48;
+while (number < 58)
+{
+putchar(number);
+number++;
+}
+putchar('\n');
+return (0);
+}
