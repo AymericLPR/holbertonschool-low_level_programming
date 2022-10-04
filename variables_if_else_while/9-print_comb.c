60940030854dab03d49594df0c7b2f1f2345cde7
AymericLPR <mric31400@gmail.com>
2022-10-04 05:24:28 -0700

    task 9 C var/if

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number = 48;
+while (number < 57)
+{
+putchar(number);
+putchar(',');
+putchar(' ');
+number++;
+}
+if (number ==57)
+{
+putchar(number);
+}
+putchar('\n');
+return (0);
+}
