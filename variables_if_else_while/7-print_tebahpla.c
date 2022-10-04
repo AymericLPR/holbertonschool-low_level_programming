065e590113900d7a226f8d8ed9f0dcdf8f7fac33
AymericLPR <mric31400@gmail.com>
2022-10-04 04:49:23 -0700

    task 7 C var/if

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int alpha = 122;
+while (alpha > 96)
+{
+putchar(alpha);
+alpha--;
+}
+putchar('\n');
+return (0);
+}
