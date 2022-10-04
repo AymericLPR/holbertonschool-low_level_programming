4da6948ba32f749ddcb23764dffc6c7ddd09dfe6
AymericLPR <mric31400@gmail.com>
2022-10-04 06:25:28 -0700

    task 1 C function/loop

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int alpha = 97;
+while (alpha < 123)
+{
+_putchar(alpha);
+alpha++;
+}
+_putchar('\n');
+return (0);
+}
