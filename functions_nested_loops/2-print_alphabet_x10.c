dd7a3790e88afa90fe687354bec97b5fd431785b
AymericLPR <mric31400@gmail.com>
2022-10-04 06:52:01 -0700

    task 2 C function/loop

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+{
+int num = 0;
+int alpha = 97;
+while (num < 10)
+{
+if (alpha == 123)
+alpha = 97
+while (alpha < 123)
+{
+_putchar(alpha);
+alpha++;
+}
+num++;
+}
+_putchar('\n');
+}
