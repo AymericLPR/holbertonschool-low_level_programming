ab98e36ad4509309bc11e9ede23b63a63629fe9a
AymericLPR <mric31400@gmail.com>
2022-10-05 00:29:27 -0700

    task 3 C function/loop

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,13 +5,10 @@
  */
 int _islower(int c)
 {
-int alpha = 97;
-while (alpha < 123)
+if (c > 96 && c < 124)
 {
-putchar(alpha);
-alpha++;
-if (alpha != 99)
-return (0);
-else
 return (1);
+}
+else
+return (0);
 }
