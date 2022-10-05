e80f9180b1bd7737494d95417ff99fdec70c8092
AymericLPR <mric31400@gmail.com>
2022-10-05 00:38:50 -0700

    task 4 C function/loop

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * Return: 0 (Success)
+ * @c: variable not a letter
+ */
+int _isalpha(int c);
+{
+if ((c > 96 && c < 124) || (c > 64 && c < 91))
+{
+return (1);
+}
+else
+return (0);
+}
