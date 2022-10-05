7b7be4f63ad58c2953d2115efc8a7adafa05a9b5
AymericLPR <mric31400@gmail.com>
2022-10-05 04:52:52 -0700

    task 9 function/loop

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * times_table - Entry point
+ * Return: 0 (Success)
+ */
+void times_table(void)
+{
+int x = '0';
+int y = '0';
+int z;
+
+while (y != '9' || x != '9')
+{
+if (x != '9')
+{
+z = x * y;  
+_putchar(z);
+_putchar(',');
+_putchar(' ');
+x + 1;
+}
+else
+{
+z = x * y;
+_putchar(z);
+_putchar('\n');
+x = 0;
+y + 1;
+}
+}
+}
