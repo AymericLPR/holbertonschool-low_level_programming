789d93726bc410f9a0985134d0621878df519e72
AymericLPR <mric31400@gmail.com>
2022-10-05 00:53:41 -0700

    task 5 C function/loop

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * Return: 0 (Success)
+ * @c: variable not a letter
+ */
+int print_sign(int n)
+{
+if (n < 0)
+{
+return (-1);
+putchar('-');
+}
+else if (n == 0)
+{
+return (0);
+putchar(0);
+}
+else
+{
+return (+1);
+putchar('+');
+}
+}
