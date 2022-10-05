f0e74e45a3320de1c97964b7c8000802a769ebe5
AymericLPR <mric31400@gmail.com>
2022-10-05 01:31:09 -0700

    task 7 C function/loop

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * Return: 0 (Success)
+ * @ld: last digit
+ */
+int print_last_digit(int ld);
+{
+int n;
+if (ld % 10 > 0)
+{
+n=((ld % 10) * -1)
+}
+printf(%d, ld % 10);
+return (%d);
+}
