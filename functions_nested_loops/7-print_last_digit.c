4bbae67711f275abc1c9924d83d5846bc9858512
AymericLPR <mric31400@gmail.com>
2022-10-06 06:18:27 -0700

    task 7 C function/loop

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,25 @@
 int print_last_digit(int ld)
 {
 int n;
-if (ld % 10 > 0)
+int n2;
+
+if (ld > 0)
 {
-n=((ld % 10) * -1)
+n = (ld % 10);
+n2 = n + '0';
+_putchar(n);
+return (n);
 }
-printf(%d, ld % 10);
-return (%d);
+else if (ld < 0)
+{
+n = ((ld % -10) * -1);
+n2 = n + '0';
+_putchar(n2);
+return (n);
+}
+else
+{
+_putchar('0');
+return (0);
+}
 }
