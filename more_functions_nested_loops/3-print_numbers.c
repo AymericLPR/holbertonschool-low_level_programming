6ebdf512235c1a24837fca085bfcb1cb4cc59f7e
AymericLPR <mric31400@gmail.com>
2022-10-07 06:06:15 -0700

    task 3 C loop2

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ * Return: 0 (Success)
+ * @n: number
+ */
+void print_numbers(void)
+{
+int n = 48;
+while (n < 58)
+{
+_putchar(n)
+n++;
+}
+_putchar('\n');
+}
