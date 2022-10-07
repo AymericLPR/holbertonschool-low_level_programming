e54fc66b603a6a4f6624a13316b2c2fbaa1196f7
AymericLPR <mric31400@gmail.com>
2022-10-07 05:56:12 -0700

    task 1 C loop2

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @c: character
+ * Return: 1 if c is a digit, else 0
+ */
+
+int _isdigit(int c)
+{
+if (c > 47 && c < 58)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
