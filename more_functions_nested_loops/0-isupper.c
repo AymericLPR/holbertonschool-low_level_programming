1792b386bbfe27acc19ebe927fd792082411266e
AymericLPR <mric31400@gmail.com>
2022-10-07 05:50:10 -0700

    task 0 C loop2

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ * @c: character
+ * Return: 1 if c is UPPERCASE, else 0
+ */
+
+int _isupper(int c)
+{
+  if (c > 64 && c < 91)
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
+}
