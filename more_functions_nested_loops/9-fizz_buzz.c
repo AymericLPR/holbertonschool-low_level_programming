253ce5fdac7a6c5b6747e21bc3215403b07aba4b
AymericLPR <mric31400@gmail.com>
2022-10-08 10:27:46 -0700

    task 9 C loop2

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,26 +7,26 @@
  */
 int main(void)
 {
-int n = 1;
-while ( n < 101)
-{
-if (n % 3 == 0)
-{
-printf(" Fizz");
-}
-else if (n % 5 == 0)
-{
-printf(" Buzz");
-}
-else if ((n % 3 == 0) && (n % 5 == 0))
-{
-printf(" FizzBuzz");
-}
-else
-{
-printf(" %d", n);
-}
-printf("\n");
-return (0);
-}
+  int n = 1;
+  while ( n < 101)
+    {
+      if (n % 3 == 0)
+	{
+	  printf(" Fizz");
+	}
+      else if (n % 5 == 0)
+	{
+	  printf(" Buzz");
+	}
+      else if ((n % 3 == 0) && (n % 5 == 0))
+	{
+	  printf(" FizzBuzz");
+	}
+      else
+	{
+	  printf(" %d", n);
+	}
+      printf("\n");
+      return (0);
+    }
 }
