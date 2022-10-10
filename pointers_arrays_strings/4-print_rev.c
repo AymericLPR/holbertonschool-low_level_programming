f51a0ff452c181477c71d12d8cf94bdf0657bcc4
AymericLPR <mric31400@gmail.com>
2022-10-10 05:29:10 -0700

    task 4 C pointer

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ * @s: charater
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+int i;
+for (i = i - 1; s[i] != '\0'; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
