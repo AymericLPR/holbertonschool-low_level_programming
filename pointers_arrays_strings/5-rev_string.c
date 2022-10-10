70c59cabb9c5fe449d2417f5c6c51ec19e37882b
AymericLPR <mric31400@gmail.com>
2022-10-10 05:50:27 -0700

    task 5 C pointer

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rev_string - Entry point
+ * @s: charater
+ * Return: Always 0
+ */
+
+void rev_string(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+for (i = i - 1; s[i] != '\0'; i--)
+{
+}
+}
