2bf3072ecedc1212814b90818bce605fb46a7ee2
AymericLPR <mric31400@gmail.com>
2022-10-11 01:17:58 -0700

    task 8 C pointer

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+include "main.h"
+
+/**
+ * print_array - Entry point
+ * @a: array
+ * @n: number
+ * Return: Always 0
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+i = 0;
+
+while (i < n)
+{
+if (i < n - 1)
+{
+printf("%d, ", a[i]);
+}
+else
+{
+printf("%d", a[i]);
+}
+}
+_putchar('\n');
+}
