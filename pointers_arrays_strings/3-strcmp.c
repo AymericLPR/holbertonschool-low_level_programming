777314325feec8f706a9cc1569ceafbecc2bad40
AymericLPR <mric31400@gmail.com>
2022-10-12 07:43:32 -0700

    task 3 C pointer2

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,13 +4,13 @@
  * _strcmp - compares two given strings
  * @s1: string
  * @s2: string
- * Return: Always 0 
+ * Return: Always 0
  */
 int _strcmp(char *s1, char *s2)
 {
 int x;
 
-while(s1[x] == s2[x])
+while(s1[i] && s2[i] && s1[x] == s2[x])
 x++;
 return (s1[x] - s2[x]);
 }
