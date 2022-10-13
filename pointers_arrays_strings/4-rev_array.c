86ddc95fc275c0a2b8a3ae9640d49d1a954144b7
AymericLPR <mric31400@gmail.com>
2022-10-13 05:47:35 -0700

    task 4 C pointer2

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two given strings
+ * @n: number
+ * @a: number
+ * Return: Always 0
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+for (i = 0; a[i] != n; i++)
+{
+}
+for (i = i - 1; a[i] != n; i--)
+{
+_putchar(a[i]);
+}
+_putchar('\n');
+}
