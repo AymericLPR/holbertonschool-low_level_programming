c1566cebe47c16b48921ff0876e09b9b983effc5
AymericLPR <mric31400@gmail.com>
2022-10-13 13:04:55 -0700

    task 6 C pointer2

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - Entry point
+ * @s: string
+ * Return: Always 0
+ */
+
+char *cap_string(char *s)
+{
+int i;
+
+while (s[i] != '\0')
+{
+if (s[i] > 96 && s[i] < 123)
+{
+if (s[i - 1] == ' ' || s[i - 1] == '\t' ||
+s[i - 1] == '\n' || s[i - 1] == ',' ||
+s[i - 1] == ';' || s[i - 1] == '.' ||
+s[i - 1] == '!' || s[i - 1] == '?' ||
+s[i - 1] == '"' || s[i - 1] == '(' ||
+s[i - 1] == ')' || s[i - 1] == '{' ||
+s[i - 1] == '}' || i = 0)
+{
+s[i] = s[i] - 32;
+}
+}
+i++;
+}
+return (s);
+}
