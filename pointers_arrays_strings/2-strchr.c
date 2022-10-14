d5f70149fce258dca0a0a5bda882e2578bfda61d
AymericLPR <mric31400@gmail.com>
2022-10-14 02:54:59 -0700

    task 2 C pointer3

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Entry point
+ * @s: string
+ * @c: character
+ * Return: Always 0
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+return (s)
+}
+i++;
+}
+return (NULL);
+}
