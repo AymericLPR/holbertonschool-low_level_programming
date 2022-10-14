3440a9422227dc63f5d8bb9065bf0a5e9e64f85f
AymericLPR <mric31400@gmail.com>
2022-10-14 03:08:21 -0700

    task 2 C pointer3

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,13 @@ char *_strchr(char *s, char c)
 {
 int i = 0;
 
- while (s[i++])
-   {
-     if (s[i] == c)
-       {
-	 return (s + i);
-       }
-     i++;
-   }
- return ('\0');
+while (s[i++])
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+i++;
+}
+return ('\0');
 }
