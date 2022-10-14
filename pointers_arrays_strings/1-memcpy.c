742379e1d9ce72a8726982f5f8227c56b81aab5b
AymericLPR <mric31400@gmail.com>
2022-10-14 02:14:17 -0700

    task 1 C pointer3

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memcpy - Entry point
+ * @dest: pointer
+ * @n: number
+ * @src: pointer
+ * Return: Always 0
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i =0;
+
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (s);
+}
