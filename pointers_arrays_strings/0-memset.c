63e9a823fd58c182857053fa9c7cdb08fb7adcc8
AymericLPR <mric31400@gmail.com>
2022-10-14 01:54:08 -0700

    task 0 C pointer3

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ * @a: character
+ * @n: number
+ * @s: pointer
+ * Return: Always 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int i = 0;
+
+while (n < i)
+{
+i++;
+s[i] = b;
+}
+return (s);
+}
