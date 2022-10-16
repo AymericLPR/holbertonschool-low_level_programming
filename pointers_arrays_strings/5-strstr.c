0917505f2a28b06723135641b78cdda8673e737f
AymericLPR <mric31400@gmail.com>
2022-10-16 05:10:43 -0700

    task 5 C pointer3

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strstr - Entry point
+ * @haystack: string
+ * @needle: string
+ * Return: Always 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i;
+int n;
+if (*needle == '\0')
+{
+return (haystack);
+}
+for (i = 0; haystack[i]; i++)
+{
+if (haystack[i] == needle[0])
+{
+for (n = 0; haystack[i] == needle[n]; i++, n++)
+{
+if (needle[n] == '\0')
+{
+return (haystack + i - j);
+}
+}
+}
+}
+return (0);
+}
