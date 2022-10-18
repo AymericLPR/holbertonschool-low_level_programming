cee633e9cb70b0c9b27ad82e257116bf5578bdb1
AymericLPR <mric31400@gmail.com>
2022-10-18 02:53:37 -0700

    task 5 C rec

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * find_root - Entry point
+ * @n: number
+ * @i: number
+ * Return: Always 0
+ */
+
+int find_root(int n, int i)
+{
+if (i * i == n)
+{
+return (i);
+}
+else
+{
+return (-1);
+}
+return (find_root(n, i + 1):
+}
