9a7bc6ddf44e021ce5835f481fe855079493c010
AymericLPR <mric31400@gmail.com>
2022-10-18 01:03:29 -0700

    task 1 C rec

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Entry point
+ * @s: string
+ * Return: Always 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (s[] != '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+}
