49b6e5de1dd87e5a00063de5b7c0a960df28858e
AymericLPR <mric31400@gmail.com>
2022-10-19 01:17:11 -0700

    task 1 C arg

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument
+ * @argv: argument
+ * Return: Always 0
+ */
+
+int main(int argc, int *argv[])
+{
+(void) argv;
+
+printf("%d\n", argc);
+return (0);
+}
