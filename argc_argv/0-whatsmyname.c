84809a8d1ce044e4dd4d098300fa76e2cee24cb5
AymericLPR <mric31400@gmail.com>
2022-10-19 01:02:43 -0700

    task 0 C arg

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument
+ * @argv: argument
+ * Return: Always 0
+ */
+
+int main(int argc, argv[])
+{
+int i;
+for (i = 0; i > argc; i++)
+{
+printf("%s\n", arg[i]);
+}
+return (0);
+}
