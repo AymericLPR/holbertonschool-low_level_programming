57c2022bcc3bc5de64ffbcfe4686ef9fbdb49524
AymericLPR <mric31400@gmail.com>
2022-10-20 01:29:07 -0700

    task 6 C rec

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Entry point
+ * @n: number
+ * Return: Always 0
+ */
+
+int is_prime_number(int n)
+{
+  int i;
+  if (n / i == 1)
+    {
+      return (0);
+    }
+  else
+    {
+      return (1);
+    }
+  return (n);
+}
