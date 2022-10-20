5140d065cb9a0442d3e7294ec258f27a09ec9380
AymericLPR <mric31400@gmail.com>
2022-10-20 01:55:37 -0700

    task 6 C rec

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,6 +9,10 @@
 
 int prime_number_2(int n, int i)
 {
+if (i == n)
+{
+return (1);
+}
 if (n % i == 0)
 {
 return (0);
@@ -24,5 +28,5 @@ return (prime_number_2(n, i + 1));
 
 int is_prime_number(int n)
 {
-return (prime_number_2(n, 1));
+return (prime_number_2(n, 2));
 }
