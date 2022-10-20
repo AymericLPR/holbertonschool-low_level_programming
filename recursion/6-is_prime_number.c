eb7dce1e6ad875531f251376215d8ff9d1acaa3b
AymericLPR <mric31400@gmail.com>
2022-10-20 01:50:33 -0700

    task 6 C rec

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * prime_number_2 - Entry point
+ * @n: number
+ * @i: number
+ * Return: Always 0
+ */
+
+int prime_number_2(int n, int i)
+{
+if (n % i == 0)
+{
+return (0);
+}
+return (prime_number_2(n, i + 1);
+}
+
+/**
  * is_prime_number - Entry point
  * @n: number
  * Return: Always 0
@@ -8,14 +24,5 @@
 
 int is_prime_number(int n)
 {
-  int i;
-  if (n / i == 1)
-    {
-      return (0);
-    }
-  else
-    {
-      return (1);
-    }
-  return (n);
+return (prime_number_2(n, 1);
 }
