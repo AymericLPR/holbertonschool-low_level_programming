13a3b52d479250f605583ebc10c7970b178a9d0a
AymericLPR <mric31400@gmail.com>
2022-10-24 06:11:08 -0700

    task 0 C malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array - Entry point
+ * @c: character
+ * @size: size
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *p;
+p = malloc(sizeof(char) * size + 1);
+if (p ==0)
+{
+return (NULL);
+}
+
+for(i = 0; i < size; i++)
+{
+p[i] = c;
+}
+p[i] = '\0';
+return p;
+}
