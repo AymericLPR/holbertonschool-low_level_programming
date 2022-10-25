69d21dee2af711d7a7b1eaca628abf6ed3138b24
AymericLPR <mric31400@gmail.com>
2022-10-25 06:07:55 -0700

    task 4 C malloc

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - Entry point
+ * @grid: grid
+ * @height: height
+ * Return: Always 0.
+ */
+
+void free_grid(int **grid, int height)
+{
+int i;
+
+for (i = 0; i < height; i++)
+free(grid[i]);
+free(grid);
+}
