1cf431c70daeaecf252e705d3fc92efde33b70d9
AymericLPR <mric31400@gmail.com>
2022-10-25 05:34:34 -0700

    task 0 C malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,6 +13,10 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *p;
+if (size == 0)
+{
+return (NULL);
+}
 p = malloc(sizeof(char) * size + 1);
 if (p == NULL)
 {
@@ -23,5 +27,5 @@ for (i = 0; i < size; i++)
 p[i] = c;
 }
 p[i] = '\0';
- return (p);
+return (p);
 }
