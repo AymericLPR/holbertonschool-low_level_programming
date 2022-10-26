b785f933069f7e845271a734c01c059d40bb1dfd
AymericLPR <mric31400@gmail.com>
2022-10-26 01:27:58 -0700

    task 0 C malloc2

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Entry point
+ * @b: b
+ * Return: Always 0.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+int *i;
+
+i = malloc(bx);
+if (i == NULL)
+{
+exit(98);
+}
+(void)i;
+return (i);
+}
