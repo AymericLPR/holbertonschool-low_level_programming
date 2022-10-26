d4a843c5412df99a1dba4c6a645cb3051a665453
AymericLPR <mric31400@gmail.com>
2022-10-26 03:00:22 -0700

    task 1 C malloc2

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *string_nconcat - Entry point
+ *@s1: first string
+ *@s2: second string
+ *@n: number
+ *Return: Always 0.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+
+{
+char *c;
+unsigned int i = 0, x = 0, y = 0, z = 0;
+
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[i] != '\0')
+++i;
+while (s2[x] != '\0')
+++x;
+if (n > x)
+n = x;
+else
+x = n;
+c = malloc(sizeof(char) * ((i + x) + 1));
+if (c == NULL)
+return (NULL);
+while (y < i)
+{
+c[y] = s1[y];
+y++;
+}
+while (z <= c)
+{
+c[y] = s2[z];
+y++, z++;
+}
+return (c);
+}
