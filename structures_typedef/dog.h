c520039ec0cab12cfcb9dc3189867fc951b91336
AymericLPR <mric31400@gmail.com>
2022-10-31 07:42:43 -0700

    task 0 C structure

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,14 +9,16 @@
  * @owner: dog owner
  */
 
-typedef struct dog_T
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-} dog_T;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
