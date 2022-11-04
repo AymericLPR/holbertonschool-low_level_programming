24a559edaea93dc7276133d09b55938dbbe216a5
AymericLPR <mric31400@gmail.com>
2022-11-04 09:06:06 -0700

    task 0 C varaidic

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+
+/**
+ *sum_them_all - Entry point
+ *@n: number
+ *Return: Always 0.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+int sum = 0;
+va_list aptr;
+
+if (n == 0)
+return (0);
+va_start(aptr, n);
+for (i = 0; i < n; i++)
+sum += va_arg(aptr, int);
+va_end(aptr);
+return (sum);
+}
