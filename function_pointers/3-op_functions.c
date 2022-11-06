60b9005f4f79c76a1ab48e35dda6beef2c9f8ee0
AymericLPR <mric31400@gmail.com>
2022-11-06 05:19:20 -0800

    task 3-2 C function

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -4,9 +4,9 @@
 
 /**
  * op_add - addition
- * @a - first number
- * @b - second number
- * Return : Always 0.
+ * @a: first number
+ * @b: second number
+ * Retur: Always 0.
  */
 
 int op_add(int a, int b)
@@ -16,9 +16,9 @@ return (a + b);
 
 /**
  * op_sub - subtraction
- * @a - first number
- * @b - second number
- * Return : Always 0.
+ * @a: first number
+ * @b: second number
+ * Return: Always 0.
  */
 
 int op_sub(int a, int b)
@@ -28,9 +28,9 @@ return (a - b);
 
 /**
  *op_mul - multiplication
- * @a - first number
- * @b - second number
- * Return : Always 0.
+ * @a: first number
+ * @b: second number
+ * Return: Always 0.
  */
 
 int op_mul(int a, int b)
@@ -40,9 +40,9 @@ return (a * b);
 
 /**
  *op_div - division
- * @a - first number
- * @b - second number
- * Return : Always 0.
+ * @a: first number
+ * @b: second number
+ * Return: Always 0.
  */
 
 int op_div(int a, int b)
@@ -57,9 +57,9 @@ return (a / b);
 
 /**
  *op_mod - modulo
- * @a - first number
- * @b - second number
- * Return : Always 0.
+ * @a: first number
+ * @b: second number
+ * Return: Always 0.
  */
 
 int op_mod(int a, int b)
