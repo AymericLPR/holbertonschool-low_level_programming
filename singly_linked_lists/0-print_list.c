0693df07cd8c5cdb46cfd97d1e5c16d3a71bab05
AymericLPR <mric31400@gmail.com>
2022-11-15 01:17:49 -0800

    task 0 C list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_list - print nodes
+ *@h: pointer
+ *Return: Always 0.
+ */
+
+size_t print_list(const list_t *h)
+{
+int num = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+h = h->next;
+num++;
+}
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+num++;
+}
+return (num);
+}
