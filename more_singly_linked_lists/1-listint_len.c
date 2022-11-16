bd377f22a954e6906780788cf2e73b14a3ec3475
AymericLPR <mric31400@gmail.com>
2022-11-16 02:26:18 -0800

    task 1 C list2

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- *list_len - number of elements
+ *listint_len - number of elements
  *@h: pointer
  *Return: Always 0.
  */
 
-size_t list_len(const list_t *h)
+size_t listint_len(const listint_t *h)
 {
 int num = 0;
 while (h)
