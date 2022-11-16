65aba0e42e9c1db113c5742c2a0b896897cc2c9b
AymericLPR <mric31400@gmail.com>
2022-11-16 02:30:35 -0800

    task 2 C list2

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - add a node
+ * @head: head
+ * @str: string
+ * Return: Always 0.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+list_t *node;
+node = malloc(sizeof(list_t));
+if (!node)
+{
+return (NULL);
+}
+node->n = n;
+node->next = *head;
+*head = node;
+return (*head);
+}
