b3cf01083bb593a33c5f4f67e281f1b91d975b64
AymericLPR <mric31400@gmail.com>
2022-11-18 00:55:18 -0800

    task 7 C list2

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Entry point
+ * @head: head
+ * @index: index 
+ * Return: Always 0.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int i = 0;
+
+while (head)
+{
+if (i == index)
+return (head);
+head = head->next;
+i++;
+}
+return (NULL);
+}
